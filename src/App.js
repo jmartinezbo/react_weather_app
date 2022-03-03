b673a39e63f4572113da6bc2a8167869dc1ee347
jrg <jorgemartinez91@hotmail.com>
2022-03-03 16:33:00 -0400

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const api = {
+  'key':'eb838b6ea74398abe6cbc2b0cabb8a2a',
+  'base': 'api.openweathermap.org/data/2.5/'
+}
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
